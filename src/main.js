5886f2839302ec94b9105c03b40abf65f95af883
muellerluke <66047826+muellerluke@users.noreply.github.com>
2020-08-14 16:54:59 -0500

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import {
+  MdField,
+  MdCheckbox,
+  MdList,
+  MdButton,
+} from "vue-material/dist/components";
+import "vue-material/dist/vue-material.min.css";
 
-Vue.config.productionTip = false
+Vue.use(MdField);
+Vue.use(MdCheckbox);
+Vue.use(MdList);
+Vue.use(MdButton);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
